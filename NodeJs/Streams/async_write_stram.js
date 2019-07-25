945cb833e4afa9e5e40da48826f63c3d1c941f8d
juergenstodolka <info@stodolka-juergen.de>
2019-07-25 23:04:43 +0200

    Promisfy the pipeline with async/await

--- NodeJs/Streams/async_write_stram.js
+++ NodeJs/Streams/async_write_stram.js
@@ -1,46 +1,22 @@
 'use strict';
 
+const stram = require('stream');
 const fs = require('fs');
+const util = require('util');
 
 // Copy a file using streams
-// Returns: a promise
-const copyStream = () => {
+const pipeline = util.promisify(stram.pipeline);
 
-    return new Promise((resolve, reject) => {
-
-        const readableStream = fs.createReadStream('file.txt');
-        const writableStream = fs.createWriteStream('write_stream_output.txt');
-
-        readableStream.on('error', (err) => {
-            console.log(err);
-            reject(new Error('Cannot read from stream' + err));
-        });
-
-        writableStream.on('error', (err) => {
-            console.log(err);
-            reject(new Error('Cannot write to stream' + err));
-        });
-
-        readableStream.setEncoding('utf8');
-
-        readableStream.on('data', function (chunk) {
-            writableStream.write(chunk);
-        });
-
-        // all is good!
-        resolve('CopyStream: All right!');
-    });
-
-}
-
-
-const fileCopy = async () => {
+async function fileCopy() {
     try {
-        let result = await copyStream();
-        console.log('[I] fileCopy: ', result);
+        await pipeline(
+            fs.createReadStream('file.txt'),
+            fs.createWriteStream('write_stream_output.txt')
+        );
+        console.log('Pipeline succeeded');
     }
-    catch (error) {
-        console.log('[E] filecopy: ', error);
+    catch (err) {
+        console.log('Pipline failed', err);
     }
 }
 
