69fa253d757a350d73d023ade7342e90eb7883ac
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-08-02 12:04:52 +0200

    Read file using async/await

--- NodeJs/File/non-blocking-read-file.js
+++ NodeJs/File/non-blocking-read-file.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
+const util = require('util');
 
+// Default way to read a file using asnyc method
 fs.readFile("DATA.txt", 'utf8', (err, contents) => {
   if (err) {
     console.log('ERROR', err);
@@ -8,3 +10,16 @@ fs.readFile("DATA.txt", 'utf8', (err, contents) => {
 });
 
 console.log('after calling readFile');
+
+// Reading a file using async/await
+const read = util.promisify(fs.readFile);
+(async () => {
+  try {
+    const chunk = await read('DATA.txt', 'utf8');
+    console.log('-'.repeat(80));
+    console.log(chunk);
+  }
+  catch (error) {
+    console.log('ERROR', error);
+  }
+})();
