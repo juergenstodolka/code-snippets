2ff711e50bd1ac293640e19887702c36427e4c86
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-08-08 15:28:26 +0200

    A simple net-Socket client connects to simple socket server

---
+++ NodeJs/Socket/simple_socket_client.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const net = require('net');
+
+const client = net.connect({ port: 8080 }, function () {
+    console.log('connected to server!');
+});
+
+client.on('data', function (data) {
+    console.log(data.toString());
+    client.end();
+});
+
+client.on('end', function () {
+    console.log('disconnected from server');
+});
