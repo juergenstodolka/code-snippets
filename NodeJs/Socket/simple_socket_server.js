f42fe746d52ea8c47b2508feb760938acb4a39a7
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-08-08 15:27:56 +0200

    A simple net-Socket server

---
+++ NodeJs/Socket/simple_socket_server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const net = require('net');
+
+const server = net.createServer(function (connection) {
+
+    console.log('client connected');
+
+    connection.on('end', function () {
+        console.log('client disconnected');
+    });
+
+    connection.write('SERVER sends: Hello World!\r\n');
+    connection.pipe(connection);
+});
+
+server.listen(8080, function () {
+    console.log('server is listening');
+});
