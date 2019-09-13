080e09216a7b67212f3f2203e7edcddaae3bb008
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-09-13 17:48:46 +0200

    Use event-stream to prettyfy downloaded reault.

---
+++ NodeJs/Streams/eventStreamPretty.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//pretty.js
+// Run it... curl -sS registry.npmjs.org/event-stream | node eventStreamPretty.js
+
+if (!module.parent) {
+    var es = require('event-stream')
+    var inspect = require('util').inspect
+
+    process.stdin                          //connect streams together with `pipe`
+        .pipe(es.split())                  //split stream to break on newlines
+        .pipe(es.map(function (data, cb) { //turn this async function into a stream
+            cb(null
+                , inspect(JSON.parse(data)))   //render it nicely
+        }))
+        .pipe(process.stdout)              // pipe it to stdout !
+}
