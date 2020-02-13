7ec1b2d4b227b42959f80e567417cc1285df921a
juergen stodolka <juergen.stodolka@sealsystems.de>
2020-02-13 17:26:53 +0100

    add comments

--- Perl/map/map_sort_hash_values.pl
+++ Perl/map/map_sort_hash_values.pl
@@ -10,10 +10,11 @@ my %zugriffe=(
 print qq|Sort hash by values:\n|;
 print '-' x 30 . "\n";
 
+# Read the following lines from back to top
 print join "\n", 
-  map{($_->[0]). ': ' . ($_->[1])}
-  sort{$a->[1] <=> $b->[1]} 
-  map{[$_, $zugriffe{$_}] }
+  map{($_->[0]) . ': ' . ($_->[1])}  # convert each element in sorted array into strings look like 'Seite: counter' 
+  sort{$a->[1] <=> $b->[1]}          # sort annon array by values
+  map{[$_, $zugriffe{$_}] }          # create anon array with elements [key, value]
   keys %zugriffe;
 
 1;
