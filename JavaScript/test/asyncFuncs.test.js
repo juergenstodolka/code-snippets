855f9f8714ca9fedd1b350515f623a7cd794486a
juergenstodolka <info@stodolka-juergen.de>
2020-03-07 21:43:54 +0100

    Remove test only

--- JavaScript/test/asyncFuncs.test.js
+++ JavaScript/test/asyncFuncs.test.js
@@ -13,7 +13,7 @@ function someMadeUpAyncFunc(boolValue, cb) {
 }
 
 // Added the `only` tag to have only this set of tests to run
-describe.only("AsyncTest", function()  {
+describe("AsyncTest", function()  {
   it("should return `You get a sweet :)` if `true` is passed in", function(done) {
   
     someMadeUpAyncFunc(true, function(sweetCheck){
